a9b616893a45f29f25192d8d62296ba7148d0fde
astream <positive.thought1009@gmail.com>
2022-08-06 21:17:47 +0900

    get pokemon API with Promise

---
+++ src/utils/pokemon.js
@@ -0,0 +1,8 @@
+export const getAllPokemon = (url) => {
+    //Promiseは処理が終わるまで待つメゾット
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then((res) => res.json())      //fetchでデータを取得したものをjson形式にする
+        .then((data) => resolve(data)); //上記をthenのdataとして受け取り、成功した時はresolveでdataをjson形式でretunに返す
+    });
+};

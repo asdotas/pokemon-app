2a23045ddaf2ef4f68fd9e6755863b5373b1deb6
astream <positive.thought1009@gmail.com>
2022-08-07 12:12:26 +0900

    setting up loading when loading data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon } from './utils/pokemon';
 
 const App = () => {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
-  }, []);
-  return <div className="App"></div>
+  }, []);//1回だけ呼び出す場合は空の配列を指定する
+
+  return <div className="App">
+    {loading ? (
+      <h1>ロード中・・・</h1>
+    ) : <>
+    <h1>ポケモンデータを取得しました</h1>
+    </>}
+  </div>
 }
 
 export default App;

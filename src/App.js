186b39de108fb16f497c760938c610299e3139e3
astream <positive.thought1009@gmail.com>
2022-08-08 13:45:32 +0900

    Creating a navigation bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
 import { getAllPokemon , getPokemon } from './utils/pokemon';
+
 
 const App = () => {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
@@ -34,19 +36,22 @@ const App = () => {
   console.log(pokemonData);
 
   return (
-    <div className="App">
-    {loading ? (
-      <h1>ロード中・・・</h1>
-    ) : (
-      <>
-        <div className='pokemonCardContainer'>
-          {pokemonData.map((pokemon, i) => {
-            return <Card  key={i} pokemon={pokemon} />;
-          })}
-        </div> 
-      </>
-   )}
-  </div>
+    <>
+      <Navbar />
+      <div className="App">
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <>
+          <div className='pokemonCardContainer'>
+            {pokemonData.map((pokemon, i) => {
+              return <Card  key={i} pokemon={pokemon} />;
+            })}
+          </div> 
+        </>
+      )}
+      </div>
+    </>
   );
 }
 
